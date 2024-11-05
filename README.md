604e0ec87955f2a26e5e24f98ac35d81651ab327
Youssefbenarbiya <benarbiayousef@nabeul.r-iset.tn>
2024-11-05 21:41:02 +0100

    update final

--- README.md
+++ README.md
@@ -1,51 +1,16 @@
-# Dynamic QR Code Generator
+# qr_code_project
 
-This is a Flutter project that allows users to dynamically add input fields, enter data, and generate a QR code based on the provided inputs. The QR code is generated using the `qr_flutter` package.
-
-## Features
-
-- Dynamically add new input fields for various user details.
-- Enter information in fields like Name, Age, Email, and more.
-- Generate a QR code that encodes the input data.
-- Simple and clean UI built with Flutter.
-
-## Screenshots
-
-![App Screenshot](screenshot.png) <!-- Replace with actual screenshot -->
+A new Flutter project.
 
 ## Getting Started
 
-### Prerequisites
+This project is a starting point for a Flutter application.
 
-To run this project, you will need:
+A few resources to get you started if this is your first Flutter project:
 
-- Flutter SDK (version 3.24.2 or later)
-- Dart SDK (version 3.5.2 or later)
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-### Installation
-
-1. Clone the repository:
-
-    ```bash
-    git clone https://github.com/YOUR_USERNAME/dynamic-qr-generator.git
-
-2. Navigate to the project directory:
-
-    cd dynamic-qr-generator
-
-3. Install the dependencies:
-
-    flutter pub get
-
-4. Run the project:
-
-    flutter run
-
-Usage
-Click the + Add Field button to dynamically add a new input field.
-Enter the required data in the input fields.
-Press the Generate QR Code button to generate a QR code based on the input data.
-Packages Used
-flutter - For building the UI.
-qr_flutter - To generate QR codes.
-
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'screens/main_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,160 +9,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dynamic Fields Demo',
-      home: DynamicFields(),
-    );
-  }
-}
-
-class DynamicFields extends StatefulWidget {
-  @override
-  _DynamicFieldsState createState() => _DynamicFieldsState();
-}
-
-class _DynamicFieldsState extends State<DynamicFields> {
-  List<Widget> inputFields = [];
-  List<TextEditingController> controllers = [];
-  List<String> dynamicFieldNames = [];
-
-  void _addField() {
-    // Show dialog to get the field name from the user
-    TextEditingController nameController = TextEditingController();
-    TextEditingController valueController = TextEditingController();
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Add Field'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: InputDecoration(hintText: 'Enter Field Name'),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: valueController,
-                decoration: InputDecoration(hintText: 'Enter Field Value'),
-              ),
-            ],
+      title: 'QR Code App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.blue[50], // Light pink background
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue, // AppBar color
+          titleTextStyle: TextStyle(
+            color: Colors.white, // White text on AppBar
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
-          actions: [
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Add'),
-              onPressed: () {
-                if (nameController.text.isNotEmpty) {
-                  setState(() {
-                    TextEditingController fieldController = TextEditingController();
-                    controllers.add(fieldController);
-                    dynamicFieldNames.add(nameController.text);
-
-                    inputFields.add(
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              flex: 1,
-                              child: Text(nameController.text), // Field name
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: TextField(
-                                controller: fieldController,
-                                decoration: InputDecoration(hintText: 'Enter ${nameController.text}'),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                    fieldController.text = valueController.text; // Set the field value
-                  });
-                  Navigator.of(context).pop(); // Close dialog
-                }
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  // Generate a string with all the input data
-  String _getInputData() {
-    String data = '';
-    for (int i = 0; i < controllers.length; i++) {
-      data += '${dynamicFieldNames[i]}: ${controllers[i].text}\n';
-    }
-    return data;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Dynamic Input Fields')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: _addField,
-              child: Text('+ Add Field'),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: inputFields.length,
-                itemBuilder: (context, index) {
-                  return inputFields[index];
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final data = _getInputData(); // Get the input data
-                if (data.isNotEmpty) {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Text('QR Code'),
-                        content: SizedBox(
-                          width: 200,
-                          height: 200,
-                          child: QrImageView(
-                            data: data, // Pass the input data as a string
-                            version: QrVersions.auto,
-                            size: 200.0,
-                          ),
-                        ),
-                        actions: [
-                          TextButton(
-                            child: Text('Close'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              },
-              child: Text('Generate QR Code'),
-            ),
-          ],
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(
+            color: Colors.blue[800], // Darker pink text
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
+      home: MainScreen(),
     );
   }
 }
